d488e29c8becddb6ddf447b04b88629ce7c82fd5
dannysrouji <sroujidaniel@gmail.com>
2018-03-09 19:54:41 +0000

    successfully reading from database

--- Main/db_checker.py
+++ Main/db_checker.py
@@ -8,23 +8,13 @@ import json
 import requests
 import time
 import RPi.GPIO as GPIO
-import pyrebase
+'''import pyrebase'''
 
 
 CARD1 = '18004865AB9E'
 CARD2 = '19007E5E4970'
 FIREBASE_URL = 'https://twic-db.firebaseio.com'
 
-config = {
-  "apiKey": "AIzaSyAyOqg5oprERTAN8Z_aVe88mdfx9sb9l_A",
-  "authDomain": "twic-db.firebaseapp.com",
-  "databaseURL": "https://databaseName.firebaseio.com",
-  "storageBucket": "twic-db.appspot.com"
-}
-firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
-user = auth.sign_in_with_email_and_password("cmoisesele@live.com", "dbtest")
-db = firebase.database()
 def setup_leds():
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
@@ -34,32 +24,32 @@ def setup_leds():
 setup_leds()
 
 def red_led():
-    print"The red LED will turn on"
+    print ("The red LED will turn on")
     GPIO.output(23,GPIO.HIGH)
-    print"Delaying for 5 seconds"
+    print ("Delaying for 5 seconds")
     time.sleep(5)
-    print "Now we turn it off"
+    print ("Now we turn it off")
     GPIO.output(23,GPIO.LOW)
     
 def green_led():
-    print"The green LED will turn on"
+    print ("The green LED will turn on")
     GPIO.output(24,GPIO.HIGH)
-    print"Delaying for 5 seconds"
+    print ("Delaying for 5 seconds")
     time.sleep(5)
-    print "Now we turn it off"
+    print ("Now we turn it off")
     GPIO.output(24,GPIO.LOW)
-def check_db(data, card_id, fingerprint):
+def check_db(card_id, fingerprint):
     ''' This function checks the database for a valid card if and then looks up the corresponding fingerprint
      https://stackoverflow.com/questions/11700798/python-accessing-values-nested-within-dictionaries
      '''
-    #data = get_data()
+    data = get_data()
     # occurrence_of_card_id tracks if card id exists in the database
     # validateInput(data, card_id, fingerprint)
     cardIDFound = False
     occurrence_of_card_id = len(data)
     for item in data:
         occurrence_of_card_id = occurrence_of_card_id - 1
-        if item['id'] != card_id:
+        if item['card_id'] != card_id:
             if cardIDFound == False:
                 print('still searching for card id')
                 break
@@ -69,14 +59,29 @@ def check_db(data, card_id, fingerprint):
             print('card id found')
             occurrence_of_card_id = occurrence_of_card_id + 1
             if item['card_data']['fingerprint'] == fingerprint:
-                print 'Success'
+                print ('Success')
             else:
-                print 'Invalid fingerprint'
+                print ('Invalid fingerprint')
                 red_led()
     if occurrence_of_card_id == 0:
-        print 'Card ID not found'
+        print ('Card ID not found')
         red_led()
         
+def secure_get_data():
+    '''config = {
+      "apiKey": "AIzaSyAyOqg5oprERTAN8Z_aVe88mdfx9sb9l_A",
+      "authDomain": "twic-db.firebaseapp.com",
+      "databaseURL": "https://databaseName.firebaseio.com",
+      "storageBucket": "twic-db.appspot.com"
+    }
+    firebase = pyrebase.initialize_app(config)
+    auth = firebase.auth()
+    user = auth.sign_in_with_email_and_password("cmoisesele@live.com", "dbtest")
+    db = firebase.database()
+    test = db.child("test-data").get()
+    print(test.val())
+    '''
+    
 
 def get_data():
     '''
@@ -85,14 +90,18 @@ def get_data():
     that we upload to the database, we do not need need to know what these keys are
     so we can just work with the values
     '''
-    auth = firebase.auth()
-    user = auth.
+    
     # Get data from firebase
-    result = requests.get(FIREBASE_URL + '/test_data.json')
+    result = requests.get(FIREBASE_URL + '/users.json')
     # Convert data to json
     data = json.loads(result.text)
-    print(data)
-    return data.values()
+    data = data.values()
+    
+    data = step_down(data)
+    data = step_down(data)
+    return data
 
-get_data()
-print({'craig': '5'})
+def step_down(data):
+    data = data[0]
+    data = data.values()
+    return data
--- Main/twic.py
+++ Main/twic.py
@@ -8,9 +8,9 @@ import db_checker as db_
 #liam is 1 and 2
 #craig is 4
 data = {
-    'first':{'id':'19007E5E4970', 'card_data':{'fingerprint':'1'}},
-    'second':{'id':'18004865AB9E', 'card_data':{'fingerprint':'2'}},
-    'third':{'id':'19003F6B713C', 'card_data':{'fingerprint':'4'}}
+    'first':{'card_id':'19007E5E4970', 'card_data':{'fingerprint':'1'}},
+    'second':{'card_id':'18004865AB9E', 'card_data':{'fingerprint':'2'}},
+    'third':{'card_id':'19003F6B713C', 'card_data':{'fingerprint':'4'}}
               }
 def run(data):
     '''runs the card and biometric readers'''
@@ -18,6 +18,6 @@ def run(data):
     fingerprint = verify.read_fp()
     #convert database data from dictionary to list
     values = data.values()
-    db_.check_db(values, str(card_id), str(fingerprint))
+    db_.check_db(str(card_id), str(fingerprint))
 
 run(data)
